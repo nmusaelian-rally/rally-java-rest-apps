41e7fbcfa7df239818ebafb89f1c58ff057ab198
N M <nmusaelian@rallydev.com>
2014-12-05 15:54:09 -0700

    working commit

--- bulkUpdateTagsOnDefects/BulkUpdateTagsOnDefects.java
+++ bulkUpdateTagsOnDefects/BulkUpdateTagsOnDefects.java
@@ -16,8 +16,7 @@ public class BulkUpdateTagsOnDefects {
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
 		
-                 /* TODO remove old tag1*/
-		
+
 		   String host = "https://rally1.rallydev.com";
 	       String apiKey = "_abc123";
 	       String workspaceRef = "/workspace/12352608129";
@@ -42,17 +41,30 @@ public class BulkUpdateTagsOnDefects {
 	        QueryResponse defectQueryResponse = restApi.query(defectRequest);
 	        System.out.println("Size: " + defectQueryResponse.getTotalResultCount());
 	        
-        	QueryRequest  tagRequest = new QueryRequest("Tag");
-        	tagRequest.setWorkspace(workspaceRef);
-        	tagRequest.setQueryFilter(new QueryFilter("Name", "=", tagToAdd));
-        	QueryResponse tagQueryResponse = restApi.query(tagRequest);
-            if(tagQueryResponse.getTotalResultCount() == 0){
+	        /*
+	        QueryRequest  oldTagRequest = new QueryRequest("Tag");
+        	oldTagRequest.setWorkspace(workspaceRef);
+        	oldTagRequest.setQueryFilter(new QueryFilter("Name", "=", tagToRemove));
+        	QueryResponse oldTagQueryResponse = restApi.query(oldTagRequest);
+            if(oldTagQueryResponse.getTotalResultCount() == 0){
+            	System.out.println("Cannot find tag: " + tagToRemove);
+                return;
+            }
+            JsonObject oldTagJsonObject = oldTagQueryResponse.getResults().get(0).getAsJsonObject();
+ 	        String oldTagRef = oldTagJsonObject.get("_ref").getAsString();
+ 	        System.out.println(oldTagRef);
+	        */
+        	QueryRequest  newTagRequest = new QueryRequest("Tag");
+        	newTagRequest.setWorkspace(workspaceRef);
+        	newTagRequest.setQueryFilter(new QueryFilter("Name", "=", tagToAdd));
+        	QueryResponse newTagQueryResponse = restApi.query(newTagRequest);
+            if(newTagQueryResponse.getTotalResultCount() == 0){
             	System.out.println("Cannot find tag: " + tagToAdd);
                 return;
             }
-            JsonObject tagJsonObject = tagQueryResponse.getResults().get(0).getAsJsonObject();
- 	        String tagRef = tagJsonObject.get("_ref").getAsString();
- 	        System.out.println(tagRef);
+            JsonObject newTagJsonObject = newTagQueryResponse.getResults().get(0).getAsJsonObject();
+ 	        String newTagRef = newTagJsonObject.get("_ref").getAsString();
+ 	        System.out.println(newTagRef);
             
             if (defectQueryResponse.getTotalResultCount() == 0) {
              System.out.println("Cannot find defects tagged : " + tagToRemove);
@@ -70,7 +82,7 @@ public class BulkUpdateTagsOnDefects {
             		 for (int j=0;j<numberOfTags;j++){
              	        System.out.println("Tag Name: " + tags.get(j).getAsJsonObject().get("Name"));
              	    }
-            		 tags.add(tagJsonObject);
+            		 tags.add(newTagJsonObject);
             		 JsonObject defectUpdate = new JsonObject();
             		 defectUpdate.add("Tags", tags);
             		 UpdateRequest updateDefectRequest = new UpdateRequest(defectRef,defectUpdate);
