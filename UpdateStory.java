1542cb77d7bbcc32b3a8220d7ff85404d713d15b
Nick Musaelian <nmusaelian@nmusaellian-mbr.f4tech.com>
2015-02-17 10:18:12 -0700

    update PlanEstimate to 0

--- UpdateStory.java
+++ UpdateStory.java
@@ -12,77 +12,65 @@ import java.net.URISyntaxException;
 
 public class UpdateStory {
 
-	public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) throws URISyntaxException, IOException {
 
+        String host = "https://rally1.rallydev.com";
+        String apiKey = "_abc123"; 
+        String applicationName = "Find story by FormattedID"; //Use your API Key or your username/password.
+        //String workspaceRef = "/workspace/12352608219";     //Use your workspace and project ObjectIDs
+        String projectRef = "/project/12352608219";
+        RallyRestApi restApi = null;
+        restApi = new RallyRestApi(new URI(host),apiKey);
+        restApi.setApplicationName(applicationName);
+        System.out.println(restApi.getWsapiVersion());
 
-		    String host = "https://rally1.rallydev.com";
-	        String username = "user@co.com";
-	        String password = "secret";
-	        String projectRef = "/project/2222";             
-	        String workspaceRef = "/workspace/11111";         
-	        String applicationName = "Nick M: example of updating a story";
-	        //String wsapiVersion = "v2.0";
-		
-        RallyRestApi restApi = new RallyRestApi(
-        		new URI(host),
-        		username,
-        		password);
-        restApi.setApplicationName(applicationName); 
-        //restApi.setWsapiVersion(wsapiVersion);
-        System.out.println(restApi.getWsapiVersion()); //v.2.0 by default when using 2.0.2 jar and up
-      
         /**********************************FIND STORY***********************************************************/
         try{
-	        QueryRequest storyRequest = new QueryRequest("HierarchicalRequirement");
-	        storyRequest.setFetch(new Fetch("Name","Owner","UserName","PlanEstimate", "c_CustomText"));
-	        storyRequest.setLimit(1000);
-	        storyRequest.setScopedDown(false);
-	        storyRequest.setScopedUp(false);
-	        storyRequest.setWorkspace(workspaceRef);
-	        storyRequest.setProject(projectRef);
-	        storyRequest.setQueryFilter(new QueryFilter("FormattedID", "=", "US359"));
-	        
-	        QueryResponse storyQueryResponse = restApi.query(storyRequest);
-	        JsonObject storyJsonObject = storyQueryResponse.getResults().get(0).getAsJsonObject();
-	        String storyRef = storyJsonObject.get("_ref").getAsString();
-	        System.out.println("Name: " + storyJsonObject.get("Name") + " PlanEstimate: " + storyJsonObject.get("PlanEstimate"));
-	
-	        JsonObject userObject = storyJsonObject.get("Owner").getAsJsonObject().getAsJsonObject();
-	        System.out.println(userObject.get("UserName"));
-	        
-	    /*****************************************************UPDATE STORY PLAN ESTIMATE****************************************/
-	        JsonObject storyUpdate = new JsonObject();
-	        storyUpdate.addProperty("Description", "my description &()#*%");
-	        storyUpdate.addProperty("PlanEstimate", 20);
-	        storyUpdate.addProperty("c_CustomText", "this is my story...&()#*%");
-	        UpdateRequest updateStoryRequest = new UpdateRequest(storyRef,storyUpdate);
-	        UpdateResponse updateResponse = restApi.update(updateStoryRequest);
-	        if (updateResponse.wasSuccessful()) {
-				System.out.println("Successfully updated story: " + storyJsonObject.get("PlanEstimate") +
-						" to: " + storyUpdate.get("PlanEstimate"));
-				System.out.println("Successfully updated story: " + storyJsonObject.get("c_CustomText") +
-						" to: " + storyUpdate.get("CustomText"));
-				System.out.println(String.format("Updated %s", updateResponse.getObject().get("_ref").getAsString()));
-				String[] warningList;
-				warningList = updateResponse.getWarnings();
-				for (int i=0;i<warningList.length;i++) {
-				    System.out.println(warningList[i]);
-					}
-			    } else {
-			    	System.out.println("Error occurred attempting to update story: " + storyJsonObject.get("Name"));
-			    	String[] errorList;
-			    	errorList = updateResponse.getErrors();
-			    	for (int i=0;i<errorList.length;i++) {
-			    		System.out.println(errorList[i]);
-			    	}
-			    }
+            QueryRequest storyRequest = new QueryRequest("HierarchicalRequirement");
+            storyRequest.setFetch(new Fetch("Name","Owner","UserName","PlanEstimate", "c_CustomText"));
+            storyRequest.setLimit(1000);
+            storyRequest.setScopedDown(false);
+            storyRequest.setScopedUp(false);
+            //storyRequest.setWorkspace(workspaceRef);
+            storyRequest.setProject(projectRef);
+            storyRequest.setQueryFilter(new QueryFilter("FormattedID", "=", "US3074"));
+
+            QueryResponse storyQueryResponse = restApi.query(storyRequest);
+            JsonObject storyJsonObject = storyQueryResponse.getResults().get(0).getAsJsonObject();
+            String storyRef = storyJsonObject.get("_ref").getAsString();
+            System.out.println("Name: " + storyJsonObject.get("Name") + " PlanEstimate: " + storyJsonObject.get("PlanEstimate"));
+
+            JsonObject userObject = storyJsonObject.get("Owner").getAsJsonObject().getAsJsonObject();
+            System.out.println(userObject.get("UserName"));
+
+            /*****************************************************UPDATE STORY PLAN ESTIMATE****************************************/
+            JsonObject storyUpdate = new JsonObject();
+            storyUpdate.addProperty("PlanEstimate", 0); //test setting PlanEstimate to 0
+            UpdateRequest updateStoryRequest = new UpdateRequest(storyRef,storyUpdate);
+            UpdateResponse updateResponse = restApi.update(updateStoryRequest);
+            if (updateResponse.wasSuccessful()) {
+                System.out.println("Successfully updated story: " + storyJsonObject.get("PlanEstimate") +
+                        " to: " + storyUpdate.get("PlanEstimate"));
+                String[] warningList;
+                warningList = updateResponse.getWarnings();
+                for (int i=0;i<warningList.length;i++) {
+                    System.out.println(warningList[i]);
+                }
+            } else {
+                System.out.println("Error occurred attempting to update story: " + storyJsonObject.get("Name"));
+                String[] errorList;
+                errorList = updateResponse.getErrors();
+                for (int i=0;i<errorList.length;i++) {
+                    System.out.println(errorList[i]);
+                }
+            }
         }catch(Exception e){
-        	System.out.println("Exception occurred....");
-    		e.printStackTrace();
+            System.out.println("Exception occurred....");
+            e.printStackTrace();
         }
         finally{
-        	//Release all resources
-    	    restApi.close();
+            //Release all resources
+            restApi.close();
         }
-	}
+    }
 }
