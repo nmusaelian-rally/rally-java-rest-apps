4d494cb8d9207e7292b5b22ec0632aa3b89f7890
Nick Musaelian <nmusaelian@nmusaellian-mbr.local>
2015-09-30 15:38:45 -0600

    set Owner on stories and tasks

--- CreateStoriesAndTasks.java
+++ CreateStoriesAndTasks.java
@@ -11,109 +11,83 @@ import com.rallydev.rest.RallyRestApi;
 import com.rallydev.rest.request.CreateRequest;
 import com.rallydev.rest.response.CreateResponse;
 import com.rallydev.rest.util.Ref;
-
+import com.rallydev.rest.request.QueryRequest;
+import com.rallydev.rest.response.QueryResponse;
+import com.rallydev.rest.util.Fetch;
+import com.rallydev.rest.util.QueryFilter;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 
-public class CreateStoriesAndTasks {
+public class CreateEpicChildStoriesTasks {
 
-	public static void main(String[] args) throws URISyntaxException, IOException {
-		
-
-	       String host = "https://rally1.rallydev.com";
-	       String username = "user@co.com";
-	       String password = "secret";
-	        String wsapiVersion = "v2.0";
-	        String projectRef = "/project/12345";
-	        String applicationName = "RestExample_createEpicStoryStoriesTasks";
-	        
-		
-        RallyRestApi restApi = new RallyRestApi(
-        		new URI(host),
-        		username,
-        		password);
-        restApi.setWsapiVersion(wsapiVersion);
-        restApi.setApplicationName(applicationName);   
-        
-        //Date d = new Date();
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        String host = "https://rally1.rallydev.com";
+        String apiKey = "_abc123"; 
+        String applicationName = "Nick:createParentChildStoriesTasks";
+        RallyRestApi restApi = null;
         DateFormat iso = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
 
         try {
-        	for (int i=0; i<1; i++) {
+            restApi = new RallyRestApi(new URI(host),apiKey);
+            restApi.setApplicationName(applicationName);
+            String projectRef = "/project/1791266714"; 
+            QueryRequest userRequest = new QueryRequest("User");
+            userRequest.setFetch(new Fetch("UserName"));
+            userRequest.setQueryFilter(new QueryFilter("UserName", "=", "user@company.com"));
+            QueryResponse userQueryResponse = restApi.query(userRequest);
+            JsonObject userJsonObject = userQueryResponse.getResults().get(0).getAsJsonObject();
+            String userRef = userJsonObject.get("_ref").getAsString();
+            for (int i=0; i<1; i++) {
+                System.out.println("Creating a story...");
+                JsonObject newEpic = new JsonObject();
+                newEpic.addProperty("Name", "epic: " + iso.format(new Date()));
+                newEpic.addProperty("Project", projectRef);
+                newEpic.addProperty("Owner", userRef);
+                CreateRequest createRequest = new CreateRequest("hierarchicalrequirement", newEpic);
+                CreateResponse createResponse = restApi.create(createRequest);
+                if (createResponse.wasSuccessful()) {
+                    System.out.println("Created " + createResponse.getObject().get("_ref").getAsString());
+                    String epicRef = Ref.getRelativeRef(createResponse.getObject().get("_ref").getAsString());
+                    System.out.println("\nReading epic story " + epicRef);
+                    for (int j=0; j<2; j++){
+                        System.out.println("Creating a child story " + j);
+                        JsonObject newChildStory = new JsonObject();
+                        newChildStory.addProperty("Name", "story: " + iso.format(new Date()));
+                        newChildStory.addProperty("Project", projectRef);
+                        newChildStory.addProperty("Parent", epicRef);
+                        newChildStory.addProperty("Owner", userRef);
+                        CreateRequest createRequest2 = new CreateRequest("hierarchicalrequirement", newChildStory);
+                        CreateResponse createResponse2 = restApi.create(createRequest2);
+                        if (createResponse2.wasSuccessful()) {
+                            System.out.println("Created " + createResponse.getObject().get("_ref").getAsString());
+                            String childStoryRef = Ref.getRelativeRef(createResponse2.getObject().get("_ref").getAsString());
+                            System.out.println("\nReading Child Story..." + childStoryRef);
+                            for (int k=0; k<3; k++){
+                                System.out.println("Creating a task " + k + " for " + childStoryRef);
+                                JsonObject newTask = new JsonObject();
+                                newTask.addProperty("Name", "task: " + iso.format(new Date()));
+                                newTask.addProperty("Project", projectRef);
+                                newTask.addProperty("WorkProduct", childStoryRef);
+                                newTask.addProperty("Owner", userRef);
+                                CreateRequest createRequest3 = new CreateRequest("task", newTask);
+                                CreateResponse createResponse3 = restApi.create(createRequest3);
+                                if (createResponse3.wasSuccessful()) {
+                                    System.out.println("Created " + createResponse3.getObject().get("_ref").getAsString());
+                                    String taskRef = Ref.getRelativeRef(createResponse3.getObject().get("_ref").getAsString());
+                                    System.out.println("\nReading task ..." + taskRef);
+                                }
+                            }
 
-	            //Add an epic story  
-	            System.out.println("Creating a story...");
-	            JsonObject newEpic = new JsonObject();
-	            newEpic.addProperty("Name", "epic: " + iso.format(new Date()));
-	            newEpic.addProperty("Project", projectRef); 
+                        }
+                    }
 
-	             
-	            CreateRequest createRequest = new CreateRequest("hierarchicalrequirement", newEpic);
-	            CreateResponse createResponse = restApi.create(createRequest);  
-	            if (createResponse.wasSuccessful()) {
-	            	
-	            	System.out.println(String.format("Created %s", createResponse.getObject().get("_ref").getAsString()));          
-	            	
-		            //Read epic story
-		            String epicRef = Ref.getRelativeRef(createResponse.getObject().get("_ref").getAsString());
-		            System.out.println(String.format("\nReading epic story %s...",epicRef));
-		            System.out.println("Creating child stories...");
-		            for (int j=0; j<2; j++){
-		            	JsonObject newChildStory = new JsonObject();
-		            	newChildStory.addProperty("Name", "story: " + iso.format(new Date()));
-		            	newChildStory.addProperty("Project", projectRef); 
-		            	newChildStory.addProperty("Parent", epicRef); 
-
-			             
-			            CreateRequest createRequest2 = new CreateRequest("hierarchicalrequirement", newChildStory);
-			            CreateResponse createResponse2 = restApi.create(createRequest2);  
-			            if (createResponse.wasSuccessful()) {
-			            	
-			            	System.out.println(String.format("Created %s", createResponse.getObject().get("_ref").getAsString()));          
-			            	
-				            //Read child story
-				            String childStoryRef = Ref.getRelativeRef(createResponse2.getObject().get("_ref").getAsString());
-				            System.out.println(String.format("\nReading Child TestFolder %s...",childStoryRef));
-				            
-				            System.out.println("Creating a task...");
-				            
-				            for (int k=0; k<3; k++){
-				            	JsonObject newTask = new JsonObject();
-				            	newTask.addProperty("Name", "task: " + iso.format(new Date()));
-				            	newTask.addProperty("WorkProduct", childStoryRef);
-					            CreateRequest createRequest3 = new CreateRequest("task", newTask);
-					            CreateResponse createResponse3 = restApi.create(createRequest3); 
-					            if (createResponse.wasSuccessful()) {
-					            	
-					            	System.out.println(String.format("Created %s", createResponse3.getObject().get("_ref").getAsString()));          
-					            	
-						            //Read task
-						            String taskRef = Ref.getRelativeRef(createResponse3.getObject().get("_ref").getAsString());
-						            System.out.println(String.format("\nReading task %s...",taskRef));
-					            }
-				            }
-				            
-			            } 
-		            }
-		            
-	            } else {
-	            	String[] createErrors;
-	            	createErrors = createResponse.getErrors();
-	        		System.out.println("Error!");
-	            	for (int j=0; i<createErrors.length;j++) {
-	            		System.out.println(createErrors[j]);
-	            	}
-	            }
-        	}
-        	
-	
+                }
+            }
         } finally {
             //Release all resources
             restApi.close();
-        }   
-
-	} 
-	
+        }
+    }
 }
